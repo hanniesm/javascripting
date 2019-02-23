c3a07f70127f8b8e394409af9db88dfe53dfea21
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-02-23 15:45:38 +0000

    Initial commit with all my work so far

---
+++ javascripting/function.arguments.js
@@ -0,0 +1,4 @@
+function math (jump, run, scream) {
+  return run * scream + jump;
+}
+console.log(math(53, 61, 67));
---
+++ javascripting/revising-strings.js
@@ -0,0 +1,3 @@
+var pizza = 'pizza is alright';
+var pizza = javapizza.replace('alright', 'wonderful');
+console.log(pizza);
