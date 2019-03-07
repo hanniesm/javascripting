6ec2985f094ec7d7eab843b7b4b739b60f5db7f2
Hannah Sutton-Molnar <hanniesutton@gmail.com>
2019-03-07 18:00:40 +0000

    updated waldoforeach exercise

--- w1d4/waldoforeach.js
+++ w1d4/waldoforeach.js
@@ -1,15 +1,16 @@
 // The second argument/parameter is expected to be a function
 
-function findWaldo(arr) {
+function findWaldo(arr, fct) {
   arr.forEach(function(element, i) {
     if (element === "Waldo") {
-      console.log("Found Waldo at index", i);
+      fct(i)
+      // console.log("Found Waldo at index", i);
     }
   });
 }
 
-// function actionWhenFound(index) {
-//   console.log("Found Waldo at index");
-// }
+function actionWhenFound(index) {
+  console.log("Found Waldo at index", index);
+}
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"]);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
