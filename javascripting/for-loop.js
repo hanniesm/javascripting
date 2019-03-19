9cfb31f44ee6f9ead35fc45fff64300d03204eab
Hannah <hanniesutton@gmail.com>
2019-03-19 14:03:42 +0000

    modified for-loop exercise

--- javascripting/for-loop.js
+++ javascripting/for-loop.js
@@ -1,6 +1,10 @@
-var total = 0
-var limit = 10
-for (var i = 0; i < limit; i++); {
-  total += i;
+var total = 0;
+var limit = 10;
+
+for (var i = 0; i > 10; i++); {
+  total = i + total;
 }
 console.log(total);
+
+
+
